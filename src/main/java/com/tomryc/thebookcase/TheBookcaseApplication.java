4ac8dc0640eef5abf8d3f9901fa73a3b45a8ca7f
Tomasz Rycerz <tomryc@gmail.com>
2019-11-10 13:54:49 +0000

    Initial Project Add

---
+++ src/main/java/com/tomryc/thebookcase/TheBookcaseApplication.java
@@ -0,0 +1,13 @@
+package com.tomryc.thebookcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBookcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBookcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomryc/thebookcase/TheBookcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomryc.thebookcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBookcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
