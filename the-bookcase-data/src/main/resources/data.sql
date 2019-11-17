d42f5757537a07cdb181fc961567d0a751f681fe
Tomasz Rycerz <tomryc@gmail.com>
2019-11-17 23:28:00 +0100

    initialize database for h2 profile. Closes #12.

---
+++ the-bookcase-data/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (description) VALUES ('Biography');
+INSERT INTO category (description) VALUES ('Action');
+INSERT INTO category (description) VALUES ('Comic');
+INSERT INTO category (description) VALUES ('Drama');
+INSERT INTO category (description) VALUES ('Fantasy');
+INSERT INTO category (description) VALUES ('Horror');
+INSERT INTO category (description) VALUES ('Romance');
+INSERT INTO location (description) VALUES ('My Room');
+INSERT INTO location (description) VALUES ('Living Room');
+INSERT INTO location (description) VALUES ('On Loan');
